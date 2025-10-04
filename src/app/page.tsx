cec7705c2fb50672fbb8dc870e07779f5ad90115
Rohan Daddekar <rohandaddekar9@gmail.com>
2025-10-04 11:35:20 +0530

    base setup with db connection done

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const HomePage = () => {
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default HomePage;
